699e213e4d802da253878aa968602c882b3c5462
George Trieu <geocrafterserver@gmail.com>
2020-02-01 22:57:50 -0500

    Ultrasonic Sensor testing

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,16 +4,44 @@
 
 #include <Servo.h>
 
-Servo open_servo;
+Servo open_servo; //Range is from 170 (Closed) to 70 (Open)
+
+const int trigPin = D3;
+const int echoPin = D2;
 
 int pos = 0;
 
+long takeMeasurement() {
+  long duration;
+  int distance;
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 /2;
+  return distance;
+}
+
 void setup() {
   open_servo.attach(D1);
-  open_servo.write(70);
-  delay(10000);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+  open_servo.write(170);
 }
 
 void loop() {
   open_servo.write(170);
+  delay(500);
+  Serial.println(takeMeasurement());
+  delay(1000);
+  open_servo.write(70);
+  delay(500);
+  Serial.println(takeMeasurement());
 }
