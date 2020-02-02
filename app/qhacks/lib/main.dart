b869d682b7cf4512fe6353f77033835ee8032b44
George Trieu <geocrafterserver@gmail.com>
2020-02-02 03:20:46 -0500

    Added MQTT Functionality

--- app/qhacks/lib/main.dart
+++ app/qhacks/lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:mqtt_client/mqtt_client.dart';
+
+final MqttClient client = MqttClient('test.mosquitto.org', '');
+String _buttonStatus = null;
+String _garageStatus = null;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,25 +33,117 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  bool _status = false;
-  String _buttonStatus = null;
-  String _garageStatus = null;
+  bool _isOpen = false;
+  bool _isOpenOld = true;
   void _button() {
-    if(_status){
-      _buttonStatus = "Close";
-      _garageStatus = "Open";
+    const String pubTopic = 'sgarage-p';
+    final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
+    if (_isOpen) {
+      builder.addString('CLOSE');
+    } else {
+      builder.addString('OPEN');
     }
-    else if(!_status) {
-      _buttonStatus = "Open";
-      _garageStatus = "Close";
+    print('EXAMPLE::Publishing our topic');
+    client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload);
+  }
+
+  void connectMQTT() async {
+    if (client.connectionStatus.state == MqttConnectionState.connected) {
+      return;
     }
-    setState(() {
-      _counter++;
+    client.keepAlivePeriod = 20;
+
+    /// Add the unsolicited disconnection callback
+    client.onDisconnected = onDisconnected;
+    /// Add the successful connection callback
+    client.onConnected = onConnected;
+
+    final MqttConnectMessage connMess = MqttConnectMessage()
+        .withClientIdentifier('Mqtt_MyClientUniqueId')
+        .keepAliveFor(20) // Must agree with the keep alive set above or not set
+        .withWillTopic('willtopic') // If you set this you must set a will message
+        .withWillMessage('My Will message')
+        .startClean();
+    print('EXAMPLE::Mosquitto client connecting....');
+    client.connectionMessage = connMess;
+
+    /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+    /// in some circumstances the broker will just disconnect us, see the spec about this, we however eill
+    /// never send malformed messages.
+    try {
+      await client.connect();
+    } on Exception catch (e) {
+      print('EXAMPLE::client exception - $e');
+      client.disconnect();
+    }
+
+    /// Check we are connectedx`
+    if (client.connectionStatus.state == MqttConnectionState.connected) {
+      print('EXAMPLE::Mosquitto client connected');
+    } else {
+      /// Use status here rather than state if you also want the broker return code.
+      print(
+          'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
+    }
+
+    /// Ok, lets try a subscription
+    print('EXAMPLE::Subscribing to the test/lol topic');
+    const String topic = 'sgarage'; // Not a wildcard topic
+    client.subscribe(topic, MqttQos.atMostOnce);
+  }
+
+  void onDisconnected() {
+    print('disconnected - reconnecting');
+    connectMQTT();
+  }
+
+  /// The successful connect callback
+  void onConnected() {
+    print(
+        'EXAMPLE::OnConnected client callback - Client connection was sucessful');
+    listenMQTT();
+  }
+
+  void listenMQTT() {
+    client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final MqttPublishMessage recMess = c[0].payload;
+      final String pt =
+      MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+      print(pt);
+      if (pt == "0") {
+        _isOpen = false;
+      } else if (pt == "1"){
+        _isOpen = true;
+      }
+
+      if (_isOpen != _isOpenOld) {
+        _isOpenOld = _isOpen;
+        updateStatus();
+      }
+
     });
+  }
+
+  void updateStatus() {
+    if (_isOpen) {
+      _garageStatus = "OPEN";
+      _buttonStatus = "CLOSED";
+      setState(() {
+        _counter++;
+      });
+    } else {
+      _garageStatus = "CLOSED";
+      _buttonStatus = "OPEN";
+      setState(() {
+        _counter++;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    connectMQTT();
+
     return Scaffold(
 
       appBar: AppBar(
